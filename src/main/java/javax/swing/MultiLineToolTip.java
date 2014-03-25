9b9cbe80dbe02773458e3ec496df5605a149d8ac
Naimdjon Takhirov <naimdjon@gmail.com>
2014-03-25 21:07:54 +0100

    initial import

---
+++ src/main/java/javax/swing/MultiLineToolTip.java
@@ -0,0 +1,59 @@
+package javax.swing;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.Collection;
+
+/**
+ * @author Naimdjon Takhirov
+ */
+public class MultiLineToolTip extends JToolTip {
+
+    public MultiLineToolTip() {
+        setUI(new MultiLineToolTipUI());
+    }
+
+    class MultiLineToolTipUI extends javax.swing.plaf.metal.MetalToolTipUI/*CROSS PLATFORM UI*/ {
+        private String[] toolTipLines;
+
+        public void paint(Graphics g, JComponent c) {
+            FontMetrics metrics = getFontMetrics(c.getFont());
+            Dimension size = c.getSize();
+            g.setColor(c.getBackground());
+            g.fillRect(0, 0, size.width, size.height);
+            g.setColor(c.getForeground());
+            for (int i = 0; toolTipLines != null && i < toolTipLines.length; i++) {
+                g.drawString(toolTipLines[i], 3, (metrics.getHeight()) * (i + 1));
+            }
+        }
+
+        public Dimension getPreferredSize(JComponent component) {
+            final FontMetrics metrics = getFontMetrics(component.getFont());
+            final String tipText = getToolTipText((JToolTip) component);
+            final BufferedReader br = new BufferedReader(new StringReader(tipText));
+            String line;
+            int maxWidth = 0;
+            final Collection<String> lines = new java.util.ArrayList<String>();
+            try {
+                while ((line = br.readLine()) != null) {
+                    maxWidth = Math.max(maxWidth, SwingUtilities.computeStringWidth(metrics, line));
+                    lines.add(line);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            if (lines.size() > 1)
+                toolTipLines = lines.toArray(new String[lines.size()]);
+            int height = metrics.getHeight() * lines.size();
+            return new Dimension(maxWidth + 6, height + 4);
+        }
+
+        private String getToolTipText(final JToolTip c) {
+            String tipText = (c).getTipText();
+            if (tipText == null)
+                tipText = "";
+            return tipText;
+        }
+    }
+}
---
+++ src/main/java/javax/swing/MultilineTooltipExample.java
@@ -0,0 +1,43 @@
+package javax.swing;
+
+import java.awt.*;
+
+/**
+ * @author Naimdjon Takhirov
+ */
+public class MultilineTooltipExample {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame=new JFrame("Tooltip");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                JLabel label=new JLabel("Mouse over me!"){
+                    @Override
+                    public JToolTip createToolTip() {
+                        return new MultiLineToolTip();
+                    }
+                };
+                label.setToolTipText("This is a tooltip \n " +
+                        "spanning multiple lines! \n" +
+                        "More text here\n" +
+                        "and more...");
+                frame.getContentPane().add(label);
+                frame.setPreferredSize(new Dimension(600,400));
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+
+    static {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+}
