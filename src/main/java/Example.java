9aaffca9595e5892ce9729bfadefff74925c6639
Naimdjon Takhirov <naimdjon@gmail.com>
2014-03-25 21:32:08 +0100

    example and Readme.

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -4,7 +4,7 @@ import java.awt.*;
 /**
  * @author Naimdjon Takhirov
  */
-public class MultilineTooltipExample {
+public class Example {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
