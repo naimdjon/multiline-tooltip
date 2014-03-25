181087294f062554f21215c75e8c5b2fc33d61ff
Naimdjon Takhirov <naimdjon@gmail.com>
2014-03-25 21:27:17 +0100

    refactor.

---
+++ src/test/java/MultilineTooltipTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class MultilineTooltipTest {
+    JLabel label;
+    final MultiLineToolTip multiLineToolTip = new MultiLineToolTip();
+    @Before
+    public void setUp(){
+        label=new JLabel(){
+            @Override
+            public JToolTip createToolTip() {
+                return multiLineToolTip;
+            }
+        };
+        label.setToolTipText("test1\n" +
+                "test2\n" +
+                "test3\n");
+    }
+
+    @Test
+    public void testMultilineTooltipGenerates3Lines() throws Exception{
+        Dimension preferredSize =multiLineToolTip.getPreferredSize();
+        assertEquals(preferredSize,new Dimension(6,4));
+    }
+
+}
