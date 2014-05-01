6cb905b17ad6c013e33aa93958de4fcd38a51292
Naimdjon <naimdjon@gmail.com>
2014-05-01 16:21:36 +0200

    refactored example.

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -10,25 +10,38 @@ public class Example {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame frame = new JFrame("Tooltip");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLocationRelativeTo(null);
-                JLabel label = new JLabel("Mouse over me!") {
-                    @Override
-                    public JToolTip createToolTip() {
-                        return new MultiLineToolTip();
-                    }
-                };
-                label.setToolTipText("This is a tooltip \n " +
-                        "spanning multiple lines! \n" +
-                        "More text here\n" +
-                        "and more...");
-                frame.getContentPane().add(label);
-                frame.setPreferredSize(new Dimension(600, 400));
-                frame.pack();
-                frame.setVisible(true);
+                showGUI();
             }
         });
+    }
+
+    private static void showGUI() {
+        final JFrame frame = createFrame();
+        final JLabel label = new JLabel("Mouse over me!") {
+            @Override
+            public JToolTip createToolTip() {
+                return new MultiLineToolTip();
+            }
+        };
+        label.setToolTipText("This is a tooltip \n " +
+                "spanning multiple lines! \n" +
+                "More text here\n" +
+                "and more...");
+        frame.getContentPane().add(label);
+        displayFrame(frame);
+    }
+
+    private static JFrame createFrame() {
+        final JFrame frame = new JFrame("Tooltip");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        return frame;
+    }
+
+    private static void displayFrame(JFrame frame) {
+        frame.setPreferredSize(new Dimension(600, 400));
+        frame.pack();
+        frame.setVisible(true);
     }
 
     static {
